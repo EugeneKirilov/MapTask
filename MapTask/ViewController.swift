c14d412e2c5b01d3a8b482e943f50115d4a166a5
‘EugeneKirilov’ <evgeny.kirilov@innowise-group.com>
2022-12-28 14:30:42 +0300

    docs: add README.md

---
+++ MapTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapTask
+//
+//  Created by Zenya Kirilov on 28.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
